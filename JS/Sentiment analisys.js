3b9981d401ad9cc7a1a5c799fea5c442ee978cf7
yamain reisig <91064263+Cremisi181@users.noreply.github.com>
2023-04-20 00:29:00 +0200

    Initial commit

---
+++ JS/Sentiment analisys.js
@@ -0,0 +1,19 @@
+function myFunction() {
+	async function query(data) {
+		const response = await fetch(
+			"https://api-inference.huggingface.co/models/nlptown/bert-base-multilingual-uncased-sentiment",
+			{
+				headers: { Authorization: "Bearer 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" },
+				method: "POST",
+				body: JSON.stringify(data),
+			}
+		);
+		const result = await response.json();
+		return result;
+	}
+
+	query({"inputs": "I like you. I love you"}).then((response) => {
+		console.log(JSON.stringify(response));
+	});
+
+}
