e74b0f4f25132cca0e465e23271f87deb4536cec
yameentje13@gmail.com <yameentje13@gmail.com>
2023-05-03 23:50:56 +0200

    idk why but it doesnt work

--- JS/Sentiment analisys.js
+++ JS/Sentiment analisys.js
@@ -1,4 +1,45 @@
-function myFunction() {
+// Code by: Cremisi 181
+
+
+
+
+
+
+
+
+
+//https://www.w3schools.com/ai/ai_chartjs.asp link to chart.js
+
+
+
+
+// COMMENTS BELOW ARE OUTDATED AND WILL BE CHANGED SOON
+
+
+
+
+
+// the outputs an array of json objects with the following structure:
+// [[{"label":"1 star","score":0.0021950274240225554},{"label":"2 stars","score":0.00225336872972548},{"label":"3 stars","score":0.015475968830287457},{"label":"4 stars","score":0.19356276094913483},{"label":"5 stars","score":0.7865128517150879}]]
+// the first element of the array is the result for the first word, the second element is the result for the second word, etc.
+
+
+// formula works by adding adding all the scores of the 5 stars and then dividing by the number of reviews this 
+// it just happens to also work for my purposes 
+// AR = 1*a+2*b+3*c+4*d+5*e/(R) this will be the average rating of the output in a furmula where a,b,c,d,e are the scores of the 5 stars and R is the number of reviews
+// the output will be a number between 1 and 5 < i think 
+// https://calculator.academy/average-rating-calculator-star-rating/#f1p1|f2p0 link to where i got it 
+
+
+
+
+
+
+
+
+
+
+function getSentiment() {
 	async function query(data) {
 		const response = await fetch(
 			"https://api-inference.huggingface.co/models/nlptown/bert-base-multilingual-uncased-sentiment",
@@ -11,34 +52,42 @@ function myFunction() {
 		const result = await response.json();
 		return result;
 	}
-			// the input can be a string or an array of strings
 	query({"inputs": "Its okay if you dont like me. Not everyone has good taste."}).then((response) => {
 
-
-		result = (JSON.stringify(response));
-		console.log(result);
-
-
+		result = JSON.stringify(response);
+		console.log(result[0].score);  //why it no work :(
+		// json is already parsed to js array oops 
 	});
 
+	
 }
-// the outputi s an array of json objects with the following structure:
-// [[{"label":"1 star","score":0.0021950274240225554},{"label":"2 stars","score":0.00225336872972548},{"label":"3 stars","score":0.015475968830287457},{"label":"4 stars","score":0.19356276094913483},{"label":"5 stars","score":0.7865128517150879}]]
-// the first element of the array is the result for the first word, the second element is the result for the second word, etc.
-
-
-// formula works by adding adding all the scores of the 5 stars and then dividing by the number of reviews this 
-// it just happens to also work for my purposes
-// AR = 1*a+2*b+3*c+4*d+5*e/(R) this will be the average rating of the output in a furmula where a,b,c,d,e are the scores of the 5 stars and R is the number of reviews
-// the output will be a number between 1 and 5 < i think 
-// https://calculator.academy/average-rating-calculator-star-rating/#f1p1|f2p0 link to where i got it 
-
-//https://www.w3schools.com/ai/ai_chartjs.asp link to chart.js
 
 
 
-let averageRating = 1*a+2*b+3*c+4*d+5*e/(R) // R is reviews 
 
-fun
-// printing result on html for easy testing
-document.getElementById("demo").innerHTML = result;
+
+
+
+
+
+
+
+// function convertToAvrage() {
+	 
+
+// 	 var a = 
+// 	 var b = 
+// 	 var c = 
+// 	 var d = 
+// 	 var e = 
+// 	 var R = 
+// 	 var result = 1*a+2*b+3*c+4*d+5*e/(R);
+// 	 document.getElementById("demo").innerHTML = result;
+// }
+
+
+// let averageRating = 1*a+2*b+3*c+4*d+5*e/(R) // R is reviews 
+
+
+
+   
